f3d7d8726f4fa29d9f57a8358e7d70e8b312a797
jzx <945627077@qq.com>
2022-04-01 14:45:22 +0800

    '增加基本得vmbytecode实现'

--- repl/repl.go
+++ repl/repl.go
@@ -4,10 +4,10 @@ import (
 	"bufio"
 	"fmt"
 	"io"
-	"monkeygo/evaluator"
+	"monkeygo/compiler"
 	"monkeygo/lexer"
-	"monkeygo/object"
 	"monkeygo/parser"
+	"monkeygo/vm"
 )
 
 const MONKEY_FACE = `
@@ -20,8 +20,8 @@ const PROMPT = ">> "
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
-	env := object.NewEnvironment()
-	macroEnv := object.NewEnvironment()
+	// env := object.NewEnvironment()
+	// macroEnv := object.NewEnvironment()
 	for {
 		fmt.Fprintf(out, PROMPT)
 		scanned := scanner.Scan()
@@ -42,14 +42,33 @@ func Start(in io.Reader, out io.Writer) {
 			printParserErrors(out, p.Errors())
 			continue
 		}
-		evaluator.DefineMacros(program, macroEnv)
-		expanded := evaluator.ExpandMacros(program, macroEnv)
-
-		evaluated := evaluator.Eval(expanded, env)
-		if evaluated != nil {
-			io.WriteString(out, evaluated.Inspect())
-			io.WriteString(out, "\n")
+		//编译到bytecode
+		comp := compiler.New()
+		err := comp.Compile(program)
+		if err != nil {
+			fmt.Fprintf(out, "Woops! Compilation failed:\n %s\n", err)
+			continue
 		}
+		//执行bytecode
+		machine := vm.New(comp.Bytecode())
+		err = machine.Run()
+		if err != nil {
+			fmt.Fprintf(out, "Woops! Executing bytecode failed:\n %s\n", err)
+			continue
+		}
+
+		lastPopped := machine.LastPoppedStackElem()
+		io.WriteString(out, lastPopped.Inspect())
+		io.WriteString(out, "\n")
+
+		// evaluator.DefineMacros(program, macroEnv)
+		// expanded := evaluator.ExpandMacros(program, macroEnv)
+
+		// evaluated := evaluator.Eval(expanded, env)
+		// if evaluated != nil {
+		// 	io.WriteString(out, evaluated.Inspect())
+		// 	io.WriteString(out, "\n")
+		// }
 	}
 }
 func printParserErrors(out io.Writer, errors []string) {
